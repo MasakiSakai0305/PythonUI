fe539af7cbf816dc7a773bcec717b5181e0669e5
Masaki_Lab <al16046@shibaura-it.ac.jp>
2019-09-19 18:13:49 +0900

    def Anime():

--- ui_button.py
+++ ui_button.py
@@ -1,13 +1,67 @@
 import tkinter
 import sys
+import ui_gragh as gragh
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+import matplotlib.pyplot as plt
+from numpy.random import *
+import matplotlib.animation as an
+import numpy as np
+
+
+x1=[]
+y1=[]
+data=rand(100)
+
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+
+def _update(frame, x, y, data):
+    """グラフを更新するための関数"""
+    # 現在のグラフを消去する
+    plt.cla()
+    # データを更新 (追加) する
+    x.append(frame)
+    y.append(data[frame])
+
+    # 折れ線グラフを再描画する
+    #print(y)
+    #print(len(y))
+    plt.scatter(x, y)
+
+def Anime():
+    fig = plt.figure(figsize=(10, 6))
+    params = {
+        'fig': fig,
+        'func': _update,  # グラフを更新する関数
+        'fargs': (x1, y1, data),  # 関数の引数 (フレーム番号を除く)
+        'interval': 10,  # 更新間隔 (ミリ秒)
+        'frames': np.arange(0, 100, 1),  # フレーム番号を生成するイテレータ
+        'repeat': False,  # 繰り返さない
+    }
+    anime = an.FuncAnimation(**params)
+    fig.show()
+    plt.show()
+
 
 root=tkinter.Tk()
 root.title(u"Button")
 root.geometry("400x300")
 
-Button1 = tkinter.Button(text=u'Button1')
-Button2 = tkinter.Button(text=u'Button2')
+#canvas = FigureCanvasTkAgg(fig, master=root)
+#toolbar = NavigationToolbar2Tk(canvas, root)
+#canvas.get_tk_widget().pack()
+
+Button1 = tkinter.Button(text=u'show plot', master=root, command=Anime)
 Button1.pack()
+
+Button2 = tkinter.Button(text=u'Button2', master=root)
 Button2.pack()
+
+button = tkinter.Button(master=root, text="Quit", command=_quit)
+button.pack()
 
 root.mainloop()
--- ui_gragh.py
+++ ui_gragh.py
@@ -4,15 +4,13 @@ import matplotlib.animation as an
 import numpy as np
 import math
 from numpy.random import *
+import random
 
+## matplot animaion
 x1=[]
 y1=[]
 #y=np.arange(0,20,2)
 data=rand(100)
-
-x2=[9,8,7,6,5]
-y2=[7,8,9,6,5]
-
 
 def _update(frame, x, y, data):
     """グラフを更新するための関数"""
@@ -23,14 +21,15 @@ def _update(frame, x, y, data):
     y.append(data[frame])
 
     # 折れ線グラフを再描画する
-    print(y)
+    #print(y)
     #print(len(y))
     plt.scatter(x, y)
 
 #fig, ax = plt.subplots(2,1, figsize=(8,6))
-fig = plt.figure(figsize=(10, 6))
 
-params = {
+def Anime():
+    fig = plt.figure(figsize=(10, 6))
+    params = {
         'fig': fig,
         'func': _update,  # グラフを更新する関数
         'fargs': (x1, y1, data),  # 関数の引数 (フレーム番号を除く)
@@ -38,12 +37,74 @@ params = {
         'frames': np.arange(0, 100, 1),  # フレーム番号を生成するイテレータ
         'repeat': False,  # 繰り返さない
     }
-anime = an.FuncAnimation(**params)
+    anime = an.FuncAnimation(**params)
+    fig.show()
+    plt.show()
 
-#ax[1]=plt.plot(x2, y2)
 
-fig.show()
-plt.show()
+##matplot pause
+def pause_plot():
+    fig, ax = plt.subplots(1, 1)
+    X = np.arange(0,10000,1)
+    Y = [random.randint(0, 10) for i in range(10000)]
+    Y = [float(y) for y in Y]
+
+    x = np.arange(-np.pi, np.pi, 0.1)
+    y = np.sin(x)
+
+    print(len(x), len(y))
+    # 初期化的に一度plotしなければならない
+    # そのときplotしたオブジェクトを受け取る受け取る必要がある．
+    # listが返ってくるので，注意
+    lines, = ax.plot(X[:50],Y[:50])
+    print(lines)
+
+    # ここから無限にplotする
+    for i in range(200):
+        #x += 0.1
+        #y = np.sin(x)
+
+
+        #x = X[i]
+        #y = Y[i]
+        a=i*50
+        x = X[a:a+50]
+        y = Y[a:a+50]
+        print(x, y)
+
+
+        # plotデータの更新
+        # 描画データを更新するときにplot関数を使うと
+        # lineオブジェクトが都度増えてしまうので，注意．
+        #
+        # 一番楽なのは上記で受け取ったlinesに対して
+        # set_data()メソッドで描画データを更新する方法．
+        
+        lines.set_data(x, y)
+
+        # set_data()を使うと軸とかは自動設定されないっぽいので，
+        # 今回の例だとあっという間にsinカーブが描画範囲からいなくなる．
+        # そのためx軸の範囲は適宜修正してやる必要がある．
+        
+        ax.set_xlim((x.min(), x.max()))
+
+        # 一番のポイント
+        # - plt.show() ブロッキングされてリアルタイムに描写できない
+        # - plt.ion() + plt.draw() グラフウインドウが固まってプログラムが止まるから使えない
+        # ----> plt.pause(interval) これを使う!!! 引数はsleep時間
+        plt.pause(.1)
+
+    
+    
+
+if __name__ == "__main__":
+    Anime()
+    #pause_plot()
+
+
+
+
+
 
 
 """
